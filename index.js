3add4ebd6f4708dbd1668cc91321c1209a9dd1f8
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-11-17 14:14:38 +0100

    add error handling and cli options

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { Transform } = require("stream");
 const { createReadStream, createWriteStream } = require("fs");
 
+const checkFileName = file => {
+  let filetype = file.split(".")[1];
+  if (filetype !== "txt") {
+    return "not a valid filetype";
+  }
+};
+
 class ReplaceText extends Transform {
   constructor([char, regex]) {
     super();
@@ -19,26 +26,44 @@ class ReplaceText extends Transform {
   }
 
   _flush(callback) {
-    this.push("more stuff is being passed...");
+    this.push("more coming");
     callback();
   }
-}
-let secretWord = process.argv[2];
-if (!secretWord) {
-  console.log("please enter a secret word after the filename");
-  return;
-}
-const xStream = new ReplaceText(textReplace(process.argv[2]));
-const fileStream = createReadStream("./text.txt");
-const writeStream = createWriteStream("./censored.txt");
-fileStream.pipe(xStream).pipe(writeStream);
-
-function textReplace(text) {
-  const replace = text.toLowerCase();
-  const re = new RegExp(replace, "g");
-
-  return ["x".repeat(text.length), re];
 }
 
 // create a censor tool.  You enter the words you want to censor
 // the files are then censored
+
+const main = () => {
+  let fileName = process.argv[2];
+  if (!fileName) {
+    console.log("please enter a filename");
+    return;
+  }
+
+  let check = checkFileName(fileName);
+
+  if (check) {
+    console.log(check);
+    return;
+  }
+
+  let secretWord = process.argv[3];
+  if (!secretWord) {
+    console.log("please enter a secret word after the filename");
+    return;
+  }
+  console.log("hi there this is working");
+  const xStream = new ReplaceText(textReplace(secretWord));
+  const fileStream = createReadStream(`./${fileName}`);
+  const writeStream = createWriteStream("./censored.txt");
+  fileStream.pipe(xStream).pipe(writeStream);
+
+  function textReplace(text) {
+    const replace = text.toLowerCase();
+    const re = new RegExp(replace, "g");
+
+    return ["x".repeat(text.length), re];
+  }
+};
+main();
